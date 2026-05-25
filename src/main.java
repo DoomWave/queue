5f4c6b173e23a3e822b80a4cd2d79f6753dd087c
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2026-05-25 14:15:28 -0700

    Messy around the queues with stack

--- src/main.java
+++ src/main.java
@@ -1,9 +1,17 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class main {
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<String>();
+        Stack<String> stack = new Stack();
+
+        stack.push("Rodimus");
+        stack.push("Ultra Magnus");
+        stack.push("Drift");
+        stack.push("Ratchet");
+        stack.push("Perceptor");
 
         queue.offer("Rodimus");
         queue.offer("Ultra Magnus");
@@ -11,6 +19,30 @@ public class main {
         queue.offer("Ratchet");
         queue.offer("Perceptor");
 
-        System.out.println(queue);
+
+
+//        System.out.println(queue.isEmpty());
+//        System.out.println(queue.size());
+//        queue.poll();
+//        System.out.println(queue.contains(("Rodimus")));
+
+//        queue.poll();
+//        queue.poll();
+//        queue.element();
+        //System.out.println(queue.peek());
+
+//        System.out.println(queue);
+//        System.out.println(stack);
+//
+//        queue.add("Rodimus");
+//        queue.offer("Perceptor");
+//
+//        System.out.println("Head: " +  queue.peek());
+//        System.out.println("Tail: " +  queue.poll());
+//
+//        System.out.println("Queuebots: " + queue);
+//
+
+
     }
 }
